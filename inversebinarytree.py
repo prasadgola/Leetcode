3a91ef6f19adc30c76cee3cda2078b582c8c800c
prasadgola <basavaprasad.m.gola@gmail.com>
2023-08-17 23:55:02 -0700

    Inverse Binary Tree

---
+++ inversebinarytree.py
@@ -0,0 +1,10 @@
+class Solution:
+    def invertTree(self, root: Optional[TreeNode]) -> Optional[TreeNode]:
+        stack = [root]
+        while stack:
+            node = stack.pop()
+            if node:
+                node.left, node.right = node.right, node.left
+                stack.append(node.left)
+                stack.append(node.right)
+        return root
